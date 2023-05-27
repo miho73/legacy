3e58f33f7d6fa7911560e061f3d77a26707210df
Yeji <hyrexfiels@gmail.com>
2023-05-28 02:09:29 +0900

    Applied Jasypt

--- src/main/java/com/github/miho73/legacy/LegacyApplication.java
+++ src/main/java/com/github/miho73/legacy/LegacyApplication.java
@@ -1,11 +1,13 @@
 package com.github.miho73.legacy;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession
+@EnableEncryptableProperties
 public class LegacyApplication {
 
 	public static void main(String[] args) {
